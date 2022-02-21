866f7cacf26ec6762b362ac3f2b307a49c4f3581
Saayks <gabriel.dasneves785@gmail.com>
2022-02-22 00:34:40 +0100

    4 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot([{ path: '', component: ProductListComponent }
-                          { path : 'products/:productId', component: ProductDetailsComponent}]),
+                          ,{ path : 'products/:productId', component: ProductDetailsComponent}]),
   ],
   declarations: [
     AppComponent,
